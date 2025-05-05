8d0d8a782e19718254e2a06e84b82790706fc50a
Scientistt <fabio.vitor.noronha@gmail.com>
2025-05-05 12:27:08 -0400

    feat: installed chakra-ui and made some minor configs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,44 @@
-import Image from "next/image";
+import { Heading, VStack, Text } from "@chakra-ui/react";
 
 export default function Home() {
     return (
-        <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-            <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-                <Image
-                    className="dark:invert"
-                    src="/next.svg"
-                    alt="Next.js logo"
-                    width={180}
-                    height={38}
-                    priority
-                />
-                <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm/6 sm:text-left">
-                    <li className="mb-2 tracking-[-.01em]">
-                        Get started by editing{" "}
-                        <code className="rounded bg-black/[.05] px-1 py-0.5 font-[family-name:var(--font-geist-mono)] font-semibold dark:bg-white/[.06]">
-                            src/app/page.tsx
-                        </code>
-                        .
-                    </li>
-                    <li className="tracking-[-.01em]">
-                        Save and see your changes instantly.
-                    </li>
-                </ol>
+        <>
+            <VStack align="center" pt="10%" pl="30%" pr="30%">
+                <Heading fontSize="5xl">🌿 EcoDrive</Heading>
+                <Text fontWeight="bold" mt="10px">
+                    Armazene com consciência. Use com liberdade.
+                </Text>
+                <Text textAlign="justify" fontWeight="normal" mt="10px">
+                    O EcoDrive é um sistema de armazenamento pessoal open source baseado em buckets (como os da AWS S3),
+                    que organiza arquivos em tiers de armazenamento para otimizar espaço e reduzir custos. Ideal para
+                    quem lida com muitos arquivos e precisa de uma solução eficiente, elegante e personalizável.
+                </Text>
 
-                <div className="flex flex-col items-center gap-4 sm:flex-row">
-                    <a
-                        className="bg-foreground text-background flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent px-4 text-sm font-medium transition-colors hover:bg-[#383838] sm:h-12 sm:w-auto sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            className="dark:invert"
-                            src="/vercel.svg"
-                            alt="Vercel logomark"
-                            width={20}
-                            height={20}
-                        />
-                        Deploy now
-                    </a>
-                    <a
-                        className="flex h-10 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm font-medium transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:w-auto sm:px-5 sm:text-base md:w-[158px] dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
-                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Read our docs
-                    </a>
-                </div>
-            </main>
-            <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/file.svg"
-                        alt="File icon"
-                        width={16}
-                        height={16}
-                    />
-                    Learn
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/window.svg"
-                        alt="Window icon"
-                        width={16}
-                        height={16}
-                    />
-                    Examples
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/globe.svg"
-                        alt="Globe icon"
-                        width={16}
-                        height={16}
-                    />
-                    Go to nextjs.org →
-                </a>
-            </footer>
-        </div>
+                <Text fontWeight="normal" mt="10px" textAlign="justify">
+                    O EcoDrive nasceu da necessidade pessoal de guardar muitos arquivos sem sobrecarregar o SSD nem
+                    depender de soluções comerciais e caras. Criado com amor, ele busca unir eficiência, economia e
+                    liberdade. Mais que um drive, o EcoDrive é um lar consciente para os seus dados.
+                </Text>
+                <Text mt="30px">
+                    💡 Para saber mais acesse:
+                    <b>
+                        <a href="https://github.com/Scientistt/ecoDrive-web-reactjs/">
+                            github.com/Scientistt/ecoDrive-web-reactjs
+                        </a>
+                    </b>
+                </Text>
+
+                <Text mt="10px">Com carinho,</Text>
+
+                <Text textAlign="center">
+                    Fabio Vitor<br></br>
+                    <b>
+                        <a href="https://github.com/Scientistt">@Scientistt</a>
+                    </b>
+                    <br></br>
+                    Criador e mantenedor do 🌿EcoDrive
+                </Text>
+            </VStack>
+        </>
     );
 }
