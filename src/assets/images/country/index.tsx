e8261e0559c1b06642ee61cb13007669e1e822b1
Scientistt <fabio.vitor.noronha@gmail.com>
2025-05-05 22:26:03 -0400

    feat: created some assets for countries, files and directories

---
+++ src/assets/images/country/index.tsx
@@ -0,0 +1,53 @@
+import australia from "assets/images/country/australia.png";
+import bahrain from "assets/images/country/bahrain.png";
+import brazil from "assets/images/country/brazil.png";
+import canada from "assets/images/country/canada.png";
+import china from "assets/images/country/china.png";
+import emirates from "assets/images/country/emirates.png";
+import england from "assets/images/country/england.png";
+import france from "assets/images/country/france.png";
+import germany from "assets/images/country/germany.png";
+import hong_kong from "assets/images/country/hong_kong.png";
+import india from "assets/images/country/india.png";
+import indonesia from "assets/images/country/indonesia.png";
+import ireland from "assets/images/country/ireland.png";
+import italy from "assets/images/country/italy.png";
+import japan from "assets/images/country/japan.png";
+import singapore from "assets/images/country/singapore.png";
+import south_africa from "assets/images/country/south_africa.png";
+import south_korea from "assets/images/country/south_korea.png";
+import spain from "assets/images/country/spain.png";
+import sweden from "assets/images/country/sweden.png";
+import switzerland from "assets/images/country/switzerland.png";
+import united_states from "assets/images/country/us.png";
+
+import blank from "assets/images/country/blank.png";
+
+const countryImages = {
+    australia: australia,
+    bahrain: bahrain,
+    brazil: brazil,
+    canada: canada,
+    china: china,
+    emirates: emirates,
+    england: england,
+    france: france,
+    germany: germany,
+    hong_kong: hong_kong,
+    india: india,
+    indonesia: indonesia,
+    ireland: ireland,
+    italy: italy,
+    japan: japan,
+    singapore: singapore,
+    south_africa: south_africa,
+    south_korea: south_korea,
+    spain: spain,
+    sweden: sweden,
+    switzerland: switzerland,
+    united_states: united_states,
+
+    blank: blank
+};
+
+export default countryImages;
---
+++ src/assets/images/index.tsx
@@ -0,0 +1,1 @@
+export { default as countryImages } from "./country";
