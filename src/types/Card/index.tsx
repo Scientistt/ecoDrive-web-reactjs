187a764ff36ff71b2804e58b6547b7308d2684ac
Scientistt <fabio.vitor.noronha@gmail.com>
2025-05-07 01:37:05 -0400

    feat: defined some new types

--- src/types/Card/index.tsx
+++ src/types/Card/index.tsx
@@ -1,5 +1,13 @@
-import { type CardRootProps } from "@chakra-ui/react";
+import { type CardRootProps, type StackProps } from "@chakra-ui/react";
+import { Bucket, BucketObject } from "types";
 
 export interface BucketCardProps extends CardRootProps {
     bucketName: string;
+}
+
+export interface BucketHeadingProps extends CardRootProps {
+    bucket: Bucket;
+}
+export interface BucketObjectProps extends StackProps {
+    bucketObject: BucketObject;
 }
